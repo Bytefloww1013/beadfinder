c4f170a74a4279c868712abe69eb0de73056c988
Bytefloww <jshope1013@gmail.com>
2026-08-30 15:39:45 -0400

    Restore factory: index.ts is a thin default-export wrapper

--- adapters/ohmypi/extensions/beadfinder/index.ts
+++ adapters/ohmypi/extensions/beadfinder/index.ts
@@ -3,24 +3,8 @@
  * Default-export hook factory. Loaded from .omp/extensions/beadfinder/.
  */
 import type { HookAPI } from "@oh-my-pi/pi-coding-agent/extensibility/hooks";
-import { asIssues, formatSnapshot, isAppendDecision, isClaimNext, isClosedStatus, isSessionBoot, issueId, labelsOf, parseClaimNextArgs, rememberScriptContext, runBd } from "./lib/bd.ts";
-import { hooksDisabled } from "./lib/fsutil.ts";
-import { advisor, debugEnabled, debugLog } from "./lib/log.ts";
-import { isLikelyAdrPath, isProductPath, isProtectedPath, isTrackerSidecar } from "./lib/paths.ts";
-import { loadState, recordClosed, saveState, type Persona } from "./lib/state.ts";
-import {
-  bashCommand,
-  firstBdInvocation,
-  flagValue,
-  hasFlag,
-  inputPath,
-  isBashTool,
-  isReadTool,
-  isSpawnTool,
-  isWriteTool,
-  labelBlob,
-  looksLikeProductWriteBash,
-  spawnText,
-  toolName,
-} from "./lib/tools.ts";
-import { registerDebug } from "./lib/debug.ts";
+import { createBeadfinder } from "./lib/policy.ts";
+
+export default function beadfinder(pi: HookAPI): void {
+  createBeadfinder(pi);
+}
